596731cdbe541d89c7f35ed9be9fc0765fe6a861
Soumyadeep Dawn <soumyadeepdawn2002@gmail.com>
2022-10-07 22:24:45 +0530

    first commit

---
+++ 06_sentiment_text_analysis.py
@@ -0,0 +1,28 @@
+# import nltk
+# nltk.download('punkt')
+from textblob import TextBlob
+from newspaper import Article
+
+url = "https://en.wikipedia.org/wiki/Mathematics"
+
+article = Article(url) # to make the url an article
+
+article.download() # to download html content of the links
+article.parse() # to retreive all the htmls from the article
+article.nlp() # to make it prepare for the natural language processing or for keyword extraction from the html that we received from the link
+
+text = article.summary
+print(text)
+
+blob = TextBlob(text)
+sentiment = blob.sentiment.polarity
+# here polarity defines that if it is -1.0 the text is a negative one and if it is in the side of 0 then neutral one and if it is near 1 then positive one 
+# Return a tuple of form (polarity, subjectivity ) where polarity is a float within the range [-1.0, 1.0] and subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very subjective. :rtype: namedtuple of the form Sentiment(polarity, subjectivity)
+print(sentiment)
+
+with open('senti.txt','r') as f:
+    text2 = f.read()
+
+blob2 = TextBlob(text2)
+sentiment2 = blob2.sentiment.polarity
+print(sentiment2)
